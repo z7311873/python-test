7349874172b0cc4df8785318b56e13ff018fb84c
z7311873 <33269587+z7311873@users.noreply.github.com>
2023-10-13 14:14:03 +0800

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+print("sdfsdf")
